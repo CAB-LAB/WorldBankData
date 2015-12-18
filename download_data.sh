37bd3d1bf25c64e570b642e0cdf8d86d5cfe3e58
gdkrmr <gkraemer@bgc-jena.mpg.de>
2015-12-18 16:29:53 +0100

    first commit

---
+++ download_data.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+wget -bN http://databank.worldbank.org/data/download/WDI_csv.zip
+wget -bN http://databank.worldbank.org/data/download/Edstats_csv.zip
+wget -bN http://databank.worldbank.org/data/download/Gender_Stats_csv.zip
+wget -bN http://databank.worldbank.org/data/download/IDS_CSV.zip
+wget -bN http://databank.worldbank.org/data/download/hnp_stats_csv.zip
+wget -bN http://databank.worldbank.org/data/download/WGI_csv.zip
+wget -bN http://databank.worldbank.org/data/download/MDG_csv.zip
+wget -bN http://databank.worldbank.org/data/download/GFDD_csv.zip
+wget -bN https://finances.worldbank.org/api/views/c32c-3558/rows.csv
+wget -bN http://databank.worldbank.org/data/download/PovStats_csv.zip
+wget -bN http://databank.worldbank.org/data/download/FINDEX_csv.zip
+wget -bN http://databank.worldbank.org/data/download/SDDS_csv.zip
+wget -bN http://databank.worldbank.org/data/download/GDDS_csv.zip
+wget -bN http://databank.worldbank.org/data/download/IDA_csv.zip
